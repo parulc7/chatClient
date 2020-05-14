40f873dd4d44219502b46c9f1e075898302ce8c2
parulc7 <parul.chandel14@gmail.com>
2020-05-14 22:13:01 +0530

    Added socket.io

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 // Importing Express Framework
 const express = require('express');
+// Importing Body Parser Module
+const bodyParser = require('body-parser');
 // Creating an Express Instance
 var app = express();
+
+// Creating a HTTP Server Instance on our app for Socket.io to use
+var http = require('http').Server(app);
+// Passing the server instance to the io instance
+var io = require('socket.io')(http);
+// Express Middleware Statements -> app.use()
+// Tell express to serve the desired static files on this instance
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 // Create a message array to emulate for frontend (This has to be served by the database in next step)
 var messages = [
@@ -13,11 +25,17 @@ var messages = [
 app.get('/messages', (req, res)=>{
     res.send(messages);
 })
-
-// Tell express to serve the desired static files on this instance
-app.use(express.static(__dirname));
-
+// // Routing for /messages route - if we receive a POST request, get the data in the messages form (API for message to use in frontend)
+app.post('/messages', (req, res)=>{
+    messages.push(req.body);
+    res.sendStatus(200);
+    io.emit('new_message', req.body)
+})
+// using event hook on socket instance in server.js
+io.on('connection', (socket)=>{
+	console.log('A user was just connected');
+});
 // Create a server event on port 3000
-var server = app.listen(3000, ()=>{
+var server = http.listen(3000, ()=>{
     console.log("Server is running on 127.0.0.1:", server.address().port);
 });
